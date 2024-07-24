87419b12cee78dab05d46e6e762e2a8104428ba4
Max Vinicius <app.maxvinicius@gmail.com>
2024-07-24 11:42:01 -0300

    feat: add footer section

---
+++ src/components/layout/footer-section.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const FooterSection = () => {
+  return (
+    <footer className="py-4 text-center">
+      <p>© {new Date().getFullYear()} Max Vinicius IT.</p>
+    </footer>
+  )
+}
+
+export default FooterSection
