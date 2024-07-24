d236d1f4f40d9bd39a0eae58f8d52b7bb8d90ade
Max Vinicius <app.maxvinicius@gmail.com>
2024-07-24 11:32:49 -0300

    chore: update site info

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,18 +1,19 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
-  description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  name: "DPE-PR",
+  description: "DPE-PR - Front-end technical test",
   mainNav: [
     {
       title: "Home",
       href: "/",
     },
+    {
+      title: "Dashboard",
+      href: "/dashboard",
+    },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    github: "https://github.com/MaxFullStack",
   },
 }
