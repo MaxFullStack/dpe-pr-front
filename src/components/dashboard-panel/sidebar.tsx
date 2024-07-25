d5e7b1f4da1b17bca6a5c56e9926328ee55ea179
Max Vinicius <app.maxvinicius@gmail.com>
2024-07-25 15:53:46 -0300

    chore: update dashboard sidebar

--- src/components/dashboard-panel/sidebar.tsx
+++ src/components/dashboard-panel/sidebar.tsx
@@ -3,30 +3,29 @@ import { PanelsTopLeft } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 import { useSidebarToggle } from "@/hooks/use-sidebar-toogle"
-import { useStore } from "@/hooks/use-store"
 import { Button } from "@/components/ui/button"
 
-import { Menu } from "./menu"
-import { SidebarToggle } from "./sidebar-toggle"
+import Menu from "./menu"
+import SidebarToggle from "./sidebar-toggle"
 
 const Sidebar = () => {
-  const sidebar = useStore(useSidebarToggle, (state) => state)
+  const { isOpen, setIsOpen } = useSidebarToggle()
 
-  if (!sidebar) return null
+  if (isOpen === undefined) return null
 
   return (
     <aside
       className={cn(
         "fixed left-0 top-0 z-20 h-screen -translate-x-full transition-[width] duration-300 ease-in-out lg:translate-x-0",
-        sidebar?.isOpen === false ? "w-[90px]" : "w-72"
+        isOpen === false ? "w-[90px]" : "w-72"
       )}
     >
-      <SidebarToggle isOpen={sidebar?.isOpen} setIsOpen={sidebar?.setIsOpen} />
+      <SidebarToggle isOpen={isOpen} setIsOpen={setIsOpen} />
       <div className="relative flex h-full flex-col overflow-y-auto px-3 py-4 shadow-md dark:shadow-zinc-800">
         <Button
           className={cn(
             "mb-1 transition-transform duration-300 ease-in-out",
-            sidebar?.isOpen === false ? "translate-x-1" : "translate-x-0"
+            isOpen === false ? "translate-x-1" : "translate-x-0"
           )}
           variant="link"
           asChild
@@ -36,7 +35,7 @@ const Sidebar = () => {
             <h1
               className={cn(
                 "whitespace-nowrap text-lg font-bold transition-[transform,opacity,display] duration-300 ease-in-out",
-                sidebar?.isOpen === false
+                isOpen === false
                   ? "hidden -translate-x-96 opacity-0"
                   : "translate-x-0 opacity-100"
               )}
@@ -45,7 +44,7 @@ const Sidebar = () => {
             </h1>
           </Link>
         </Button>
-        <Menu isOpen={sidebar?.isOpen} />
+        <Menu isOpen={isOpen} />
       </div>
     </aside>
   )
