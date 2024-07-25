c730753e53dba931d62eceeffe0e526c71f8229c
Max Vinicius <app.maxvinicius@gmail.com>
2024-07-25 15:47:10 -0300

    chore: update dashboard collapse menu button

--- src/components/dashboard-panel/collapse-menu-button.tsx
+++ src/components/dashboard-panel/collapse-menu-button.tsx
@@ -1,55 +1,59 @@
-"use client";
+"use client"
 
-import Link from "next/link";
-import { useState } from "react";
-import { ChevronDown, Dot, LucideIcon } from "lucide-react";
+import { useEffect, useState } from "react"
+import Link from "next/link"
+import { DropdownMenuArrow } from "@radix-ui/react-dropdown-menu"
+import { ChevronDown, Dot, LucideIcon } from "lucide-react"
 
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import { DropdownMenuArrow } from "@radix-ui/react-dropdown-menu";
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
 import {
   Collapsible,
   CollapsibleContent,
-  CollapsibleTrigger
-} from "@/components/ui/collapsible";
-import {
-  Tooltip,
-  TooltipTrigger,
-  TooltipContent,
-  TooltipProvider
-} from "@/components/ui/tooltip";
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
 import {
   DropdownMenu,
+  DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuSeparator
-} from "@/components/ui/dropdown-menu";
+} from "@/components/ui/dropdown-menu"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 type Submenu = {
-  href: string;
-  label: string;
-  active: boolean;
-};
-
-interface CollapseMenuButtonProps {
-  icon: LucideIcon;
-  label: string;
-  active: boolean;
-  submenus: Submenu[];
-  isOpen: boolean | undefined;
+  href: string
+  label: string
+  active: boolean
 }
 
-export function CollapseMenuButton({
+interface CollapseMenuButtonProps {
+  icon: LucideIcon
+  label: string
+  active: boolean
+  submenus: Submenu[]
+  isOpen: boolean
+}
+
+const CollapseMenuButton = ({
   icon: Icon,
   label,
   active,
   submenus,
-  isOpen
-}: CollapseMenuButtonProps) {
-  const isSubmenuActive = submenus.some((submenu) => submenu.active);
-  const [isCollapsed, setIsCollapsed] = useState<boolean>(isSubmenuActive);
+  isOpen,
+}: CollapseMenuButtonProps) => {
+  const isSubmenuActive = submenus.some((submenu) => submenu.active)
+  const [isCollapsed, setIsCollapsed] = useState<boolean>(isSubmenuActive)
+
+  useEffect(() => {
+    setIsCollapsed(isSubmenuActive)
+  }, [isOpen, isSubmenuActive])
 
   return isOpen ? (
     <Collapsible
@@ -58,14 +62,14 @@ export function CollapseMenuButton({
       className="w-full"
     >
       <CollapsibleTrigger
-        className="[&[data-state=open]>div>div>svg]:rotate-180 mb-1"
+        className="mb-1 [&[data-state=open]>div>div>svg]:rotate-180"
         asChild
       >
         <Button
           variant={active ? "secondary" : "ghost"}
-          className="w-full justify-start h-10"
+          className="h-10 w-full justify-start"
         >
-          <div className="w-full items-center flex justify-between">
+          <div className="flex w-full items-center justify-between">
             <div className="flex items-center">
               <span className="mr-4">
                 <Icon size={18} />
@@ -97,16 +101,16 @@ export function CollapseMenuButton({
           </div>
         </Button>
       </CollapsibleTrigger>
-      <CollapsibleContent className="overflow-hidden data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down">
+      <CollapsibleContent className="data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down overflow-hidden">
         {submenus.map(({ href, label, active }, index) => (
           <Button
             key={index}
             variant={active ? "secondary" : "ghost"}
-            className="w-full justify-start h-10 mb-1"
+            className="mb-1 h-10 w-full justify-start"
             asChild
           >
             <Link href={href}>
-              <span className="mr-4 ml-2">
+              <span className="ml-2 mr-4">
                 <Dot size={18} />
               </span>
               <p
@@ -132,9 +136,9 @@ export function CollapseMenuButton({
             <DropdownMenuTrigger asChild>
               <Button
                 variant={active ? "secondary" : "ghost"}
-                className="w-full justify-start h-10 mb-1"
+                className="mb-1 h-10 w-full justify-start"
               >
-                <div className="w-full items-center flex justify-between">
+                <div className="flex w-full items-center justify-between">
                   <div className="flex items-center">
                     <span className={cn(isOpen === false ? "" : "mr-4")}>
                       <Icon size={18} />
@@ -172,5 +176,7 @@ export function CollapseMenuButton({
         <DropdownMenuArrow className="fill-border" />
       </DropdownMenuContent>
     </DropdownMenu>
-  );
+  )
 }
+
+export default CollapseMenuButton
