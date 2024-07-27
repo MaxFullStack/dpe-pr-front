9fda6f0754d61ad25868434cf0f8fd48c421d9e7
Max Vinicius <app.maxvinicius@gmail.com>
2024-07-27 13:59:48 -0300

    feat: add senator details api route

---
+++ app/api/senadores/[id]/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server"
+import axios from "axios"
+
+const SENATOR_DETAILS_API_URL = process.env
+  .NEXT_PUBLIC_SENATOR_DETAILS_API_URL as string
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params
+
+  if (!id) {
+    return NextResponse.json(
+      { error: "ID do senador é obrigatório" },
+      { status: 400 }
+    )
+  }
+
+  try {
+    console.log(
+      "Fetching details for senator from API:",
+      `${SENATOR_DETAILS_API_URL}/${id}`
+    )
+    const response = await axios.get(`${SENATOR_DETAILS_API_URL}/${id}`)
+    return NextResponse.json(response.data)
+  } catch (error) {
+    console.error("Error fetching details for senator:", error)
+    return NextResponse.json(
+      { error: "Failed to fetch details for senator" },
+      { status: 500 }
+    )
+  }
+}
