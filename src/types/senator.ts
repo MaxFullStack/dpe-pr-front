7ace200d44d975843409daad73d7adbffd68910d
Max Vinicius <app.maxvinicius@gmail.com>
2024-07-27 18:55:15 -0300

    feat: add types for senator

---
+++ src/types/senator.ts
@@ -0,0 +1,10 @@
+export interface Senator {
+  id: string
+  name: string
+  state: string
+  parliamentaryBlock: string
+  photo: string
+  email: string
+  phone: string
+  parliamentaryPageUrl: string
+}
