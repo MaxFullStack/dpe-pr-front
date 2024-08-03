74dd5d09f16ee412a60eb0070305e4dd7b888917
Max Vinicius <app.maxvinicius@gmail.com>
2024-08-02 21:20:16 -0300

    chore: update fetch parliamentary blocks details service

--- src/services/fetch-parliamentary-block-details.ts
+++ src/services/fetch-parliamentary-block-details.ts
@@ -5,6 +5,6 @@ import { ParliamentaryBlock } from "@/types/parliamentary-block"
 export const fetchParliamentaryBlockDetails = async (
   id: string
 ): Promise<ParliamentaryBlock> => {
-  const { data } = await axios.get(`/api/blocos-parlamentares/${id}`)
-  return data
+  const response = await axios.get(`/api/blocos-parlamentares/${id}`)
+  return response.data
 }
