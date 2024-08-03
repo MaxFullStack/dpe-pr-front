72c18799d1d9c86161fc0264d8acfb245582090b
Max Vinicius <app.maxvinicius@gmail.com>
2024-08-02 21:26:58 -0300

    feat: update parliamentary blocks table data

--- src/components/parliamentary-blocks/parliamentary-blocks-table/parliamentary-blocks-table-data.tsx
+++ src/components/parliamentary-blocks/parliamentary-blocks-table/parliamentary-blocks-table-data.tsx
@@ -15,7 +15,7 @@ export const blockColumns: ColumnDef<ParliamentaryBlock>[] = [
   {
     accessorKey: "blockCode",
     header: columnTranslations.blockCode,
-    enableHiding: false
+    enableHiding: false,
   },
 
   {
@@ -43,11 +43,11 @@ export const blockColumns: ColumnDef<ParliamentaryBlock>[] = [
   {
     accessorKey: "creationDate",
     header: columnTranslations.creationDate,
-    /*  cell: ({ row }) => {
+    cell: ({ row }) => {
       const date = new Date(row.getValue<string>("creationDate"))
       return `${date.getDate().toString().padStart(2, "0")}/${(date.getMonth() + 1).toString().padStart(2, "0")}/${date.getFullYear()}`
-    }, */
-    cell: ({ cell }) => formatDate(cell.getValue() as Date),
+    },
+    /* cell: ({ cell }) => formatDate(cell.getValue() as Date), */
   },
 ]
 
