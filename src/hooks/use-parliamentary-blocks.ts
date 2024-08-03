6e42b0b63f3c3632b8f3f86aba63f75f53d489da
Max Vinicius <app.maxvinicius@gmail.com>
2024-08-02 21:22:49 -0300

    chore: update use parliamentary blocks hook

--- src/hooks/use-parliamentary-blocks.ts
+++ src/hooks/use-parliamentary-blocks.ts
@@ -1,14 +1,34 @@
-import { useQuery, UseQueryResult } from "@tanstack/react-query"
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { fetchParliamentaryBlocks } from '@/services/fetch-parliamentary-blocks';
+import { ParliamentaryBlock } from '@/types/parliamentary-block';
 
-import { ParliamentaryBlock } from "@/types/parliamentary-block"
-import { fetchParliamentaryBlocks } from "@/services/fetch-parliamentary-blocks"
-
-export const useParliamentaryBlocks = (): UseQueryResult<
-  ParliamentaryBlock[],
-  Error
-> => {
-  return useQuery<ParliamentaryBlock[], Error>({
-    queryKey: ["parliamentary-blocks"],
-    queryFn: fetchParliamentaryBlocks,
-  })
+const transformBlocksData = (blocksData: any): ParliamentaryBlock[] => {
+  if (!blocksData) return []
+  return (
+    blocksData.ListaBlocoParlamentar?.Blocos?.Bloco?.reduce(
+      (acc: ParliamentaryBlock[], block: any) => {
+        block.Membros?.Membro?.forEach((member: any) => {
+          acc.push({
+            blockCode: block.CodigoBloco,
+            blockName: block.NomeBloco,
+            creationDate: block.DataCriacao,
+            partyAcronym: member.Partido.SiglaPartido,
+            members: [],
+          })
+        })
+        return acc
+      },
+      []
+    ) || []
+  )
 }
+
+export const useParliamentaryBlocks = (): UseQueryResult<ParliamentaryBlock[], Error> => {
+  return useQuery<ParliamentaryBlock[], Error>({
+    queryKey: ['parliamentary-blocks'],
+    queryFn: async () => {
+      const data = await fetchParliamentaryBlocks();
+      return transformBlocksData(data);
+    },
+  });
+};
