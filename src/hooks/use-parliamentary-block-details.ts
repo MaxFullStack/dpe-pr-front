ed3fb7d23fc4774fee33aeb03355893623e332f4
Max Vinicius <app.maxvinicius@gmail.com>
2024-08-02 21:23:01 -0300

    chore: update use parliamentary blocks details hook

--- src/hooks/use-parliamentary-block-details.ts
+++ src/hooks/use-parliamentary-block-details.ts
@@ -1,14 +1,39 @@
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { ParliamentaryBlock } from '@/types/parliamentary-block';
+import { fetchParliamentaryBlockDetails } from '@/services/fetch-parliamentary-block-details';
 
-import { useQuery, UseQueryResult } from "@tanstack/react-query"
+const transformBlockData = (blockData: any): ParliamentaryBlock => {
+  return {
+    blockAcronym: blockData.siglaCasa,
+    blockName: blockData.nomeBloco,
+    blockCode: blockData.codigoBloco,
+    creationDate: blockData.dataCriacao,
+    members: blockData.composicaoBloco.composicao_bloco.map((member: any) => ({
+      party: {
+        partyCode: member.partido.codigoPartido,
+        partyAcronym: member.partido.siglaPartido,
+        partyName: member.partido.nomePartido,
+      },
+      joinDate: member.dataAdesao,
+      leaveDate: member.dataDesligamento,
+    })) || [],
+  };
+};
 
-import { ParliamentaryBlock } from "@/types/parliamentary-block"
-import { fetchParliamentaryBlockDetails } from "@/services/fetch-parliamentary-block-details"
+const transformBlockDetailsData = (data: any): ParliamentaryBlock => {
+  const blockData = data.blocos.bloco;
+  console.log(blockData, "bloco data")
+  return transformBlockData(blockData);
+};
 
 export const useParliamentaryBlockDetails = (
   id: string
 ): UseQueryResult<ParliamentaryBlock, Error> => {
   return useQuery<ParliamentaryBlock, Error>({
-    queryKey: ["parliamentary-block-details", id],
-    queryFn: () => fetchParliamentaryBlockDetails(id),
-  })
-}
+    queryKey: ['parliamentary-block-details', id],
+    queryFn: async () => {
+      const data = await fetchParliamentaryBlockDetails(id);
+      return transformBlockDetailsData(data);
+    },
+  });
+};
