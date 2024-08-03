bff1ad7557885fa24e672d1b8b8c766499f62478
Max Vinicius <app.maxvinicius@gmail.com>
2024-08-02 21:23:35 -0300

    feat: update data provider context

--- src/context/data-provider.tsx
+++ src/context/data-provider.tsx
@@ -8,60 +8,22 @@ import { useParliamentaryBlockDetails } from "@/hooks/use-parliamentary-block-de
 import { useParliamentaryBlocks } from "@/hooks/use-parliamentary-blocks"
 import { useSenatorDetails } from "@/hooks/use-senator-details"
 import { useSenators } from "@/hooks/use-senators"
+import { UseQueryResult } from "@tanstack/react-query"
 
 interface DataContextProps {
-  senatorsList: Senator[]
-  parliamentaryBlocksList: ParliamentaryBlock[]
-  getParliamentaryBlockDetails: (id: string) => Promise<ParliamentaryBlock>
-  getSenatorDetails: (id: string) => Promise<Senator>
+  senatorsList: UseQueryResult<Senator[], Error>;
+  parliamentaryBlocksList: UseQueryResult<ParliamentaryBlock[], Error>;
+  getParliamentaryBlockDetails: (id: string) => UseQueryResult<ParliamentaryBlock, Error>;
+  getSenatorDetails: (id: string) => UseQueryResult<Senator, Error>;
 }
 
 const DataContext = createContext<DataContextProps | undefined>(undefined)
 
 export const DataProvider = ({ children }: { children: ReactNode }) => {
-  const { data: senatorsData } = useSenators()
-  const { data: blocksData } = useParliamentaryBlocks()
-
-  const getParliamentaryBlockDetails = async (
-    id: string
-  ): Promise<ParliamentaryBlock> => {
-    const { data } = await useParliamentaryBlockDetails(id)
-    if (!data) {
-      throw new Error("Block data not found")
-    }
-    return data
-  }
-
-  const getSenatorDetails = async (id: string): Promise<Senator> => {
-    const { data } = await useSenatorDetails(id)
-    if (!data) {
-      throw new Error("Senator data not found")
-    }
-    return data
-  }
-
-  const transformBlocksData = (blocksData: any): ParliamentaryBlock[] => {
-    if (!blocksData) return []
-  
-    return (
-      blocksData.ListaBlocoParlamentar?.Blocos?.Bloco?.reduce(
-        (acc: ParliamentaryBlock[], block: any) => {
-          block.Membros?.Membro?.forEach((member: any) => {
-            acc.push({
-              blockCode: block.CodigoBloco,
-              blockName: block.NomeBloco,
-              creationDate: block.DataCriacao,
-              partyAcronym: member.Partido.SiglaPartido,
-              members: [],
-            })
-          })
-          return acc
-        },
-        []
-      ) || []
-    )
-  }
-  
+  const parliamentaryBlocksQuery = useParliamentaryBlocks();
+  const senatorsQuery = useSenators();
+  const getParliamentaryBlockDetails = (id: string) => useParliamentaryBlockDetails(id);
+  const getSenatorDetails = (id: string) => useSenatorDetails(id);
 
   const transformSenatorsData = (senatorsData: any): Senator[] => {
     if (!senatorsData) return []
@@ -78,24 +40,14 @@ export const DataProvider = ({ children }: { children: ReactNode }) => {
     )
   }
 
-  const transformedBlocksList = useMemo(
-    () => transformBlocksData(blocksData),
-    [blocksData]
-  )
-  const transformedSenatorsList = useMemo(
-    () => transformSenatorsData(senatorsData),
-    [senatorsData]
-  )
-
-  const value = useMemo(
-    () => ({
-      senatorsList: transformedSenatorsList,
-      parliamentaryBlocksList: transformedBlocksList,
+  const value = useMemo(() => {
+    return {
+      senatorsList: senatorsQuery,
+      parliamentaryBlocksList: parliamentaryBlocksQuery,
       getParliamentaryBlockDetails,
       getSenatorDetails,
-    }),
-    [transformedSenatorsList, transformedBlocksList]
-  )
+    };
+  }, [parliamentaryBlocksQuery, senatorsQuery, getParliamentaryBlockDetails, getSenatorDetails]);
 
   return <DataContext.Provider value={value}>{children}</DataContext.Provider>
 }
