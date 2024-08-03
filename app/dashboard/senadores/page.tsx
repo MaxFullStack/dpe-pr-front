1e6ffd083efbaebc26c5f9cdb6f2780a3af9aff3
Max Vinicius <app.maxvinicius@gmail.com>
2024-08-02 21:30:34 -0300

    feat: add senators page

---
+++ app/dashboard/senadores/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import React from "react"
+
+const SenatorPage = () => {
+  return (
+    <div className="container sm:px-8">
+      <div className="mb-8">
+        <h1 className="mb-2 text-2xl font-bold">Senadores</h1>
+        <h2 className="">Lista de senadores</h2>
+      </div>
+    </div>
+  )
+}
+
+export default SenatorPage
